f5401d973ebfcb2ba80dc1d51cb53ffcd221e6b2
ohmstar123 <ohmpate114@ed.amdsb.ca>
2018-10-09 14:54:10 -0400

    hellop

--- CashRegister/Form1.Designer.cs
+++ CashRegister/Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.subLabel = new System.Windows.Forms.Label();
             this.chipsLabel = new System.Windows.Forms.Label();
             this.cookieLabel = new System.Windows.Forms.Label();
-            this.drinkLabel = new System.Windows.Forms.Label();
             this.calculatetotalButton = new System.Windows.Forms.Button();
             this.subtotalLabel = new System.Windows.Forms.Label();
             this.taxLabel = new System.Windows.Forms.Label();
@@ -42,6 +41,14 @@
             this.changeLabel = new System.Windows.Forms.Label();
             this.calculatechangeButton = new System.Windows.Forms.Button();
             this.printreceiptButton = new System.Windows.Forms.Button();
+            this.subsInput = new System.Windows.Forms.TextBox();
+            this.chipsInput = new System.Windows.Forms.TextBox();
+            this.cookiesInput = new System.Windows.Forms.TextBox();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.changeOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // titleLable
@@ -59,7 +66,7 @@
             // subLabel
             // 
             this.subLabel.AutoSize = true;
-            this.subLabel.Location = new System.Drawing.Point(39, 68);
+            this.subLabel.Location = new System.Drawing.Point(26, 54);
             this.subLabel.Name = "subLabel";
             this.subLabel.Size = new System.Drawing.Size(127, 13);
             this.subLabel.TabIndex = 1;
@@ -68,7 +75,7 @@
             // chipsLabel
             // 
             this.chipsLabel.AutoSize = true;
-            this.chipsLabel.Location = new System.Drawing.Point(39, 93);
+            this.chipsLabel.Location = new System.Drawing.Point(26, 84);
             this.chipsLabel.Name = "chipsLabel";
             this.chipsLabel.Size = new System.Drawing.Size(85, 13);
             this.chipsLabel.TabIndex = 3;
@@ -77,34 +84,26 @@
             // cookieLabel
             // 
             this.cookieLabel.AutoSize = true;
-            this.cookieLabel.Location = new System.Drawing.Point(39, 116);
+            this.cookieLabel.Location = new System.Drawing.Point(26, 115);
             this.cookieLabel.Name = "cookieLabel";
             this.cookieLabel.Size = new System.Drawing.Size(97, 13);
             this.cookieLabel.TabIndex = 4;
             this.cookieLabel.Text = "Number of Cookies";
             // 
-            // drinkLabel
-            // 
-            this.drinkLabel.AutoSize = true;
-            this.drinkLabel.Location = new System.Drawing.Point(39, 139);
-            this.drinkLabel.Name = "drinkLabel";
-            this.drinkLabel.Size = new System.Drawing.Size(89, 13);
-            this.drinkLabel.TabIndex = 5;
-            this.drinkLabel.Text = "Number of Drinks";
-            // 
             // calculatetotalButton
             // 
-            this.calculatetotalButton.Location = new System.Drawing.Point(29, 187);
+            this.calculatetotalButton.Location = new System.Drawing.Point(29, 181);
             this.calculatetotalButton.Name = "calculatetotalButton";
             this.calculatetotalButton.Size = new System.Drawing.Size(124, 23);
             this.calculatetotalButton.TabIndex = 6;
             this.calculatetotalButton.Text = "Calculate Totals";
             this.calculatetotalButton.UseVisualStyleBackColor = true;
+            this.calculatetotalButton.Click += new System.EventHandler(this.calculatetotalButton_Click);
             // 
             // subtotalLabel
             // 
             this.subtotalLabel.AutoSize = true;
-            this.subtotalLabel.Location = new System.Drawing.Point(39, 229);
+            this.subtotalLabel.Location = new System.Drawing.Point(26, 223);
             this.subtotalLabel.Name = "subtotalLabel";
             this.subtotalLabel.Size = new System.Drawing.Size(53, 13);
             this.subtotalLabel.TabIndex = 7;
@@ -113,7 +112,7 @@
             // taxLabel
             // 
             this.taxLabel.AutoSize = true;
-            this.taxLabel.Location = new System.Drawing.Point(39, 262);
+            this.taxLabel.Location = new System.Drawing.Point(26, 253);
             this.taxLabel.Name = "taxLabel";
             this.taxLabel.Size = new System.Drawing.Size(25, 13);
             this.taxLabel.TabIndex = 8;
@@ -122,7 +121,7 @@
             // totalLabel
             // 
             this.totalLabel.AutoSize = true;
-            this.totalLabel.Location = new System.Drawing.Point(39, 295);
+            this.totalLabel.Location = new System.Drawing.Point(26, 284);
             this.totalLabel.Name = "totalLabel";
             this.totalLabel.Size = new System.Drawing.Size(31, 13);
             this.totalLabel.TabIndex = 9;
@@ -131,7 +130,7 @@
             // tenderedLabel
             // 
             this.tenderedLabel.AutoSize = true;
-            this.tenderedLabel.Location = new System.Drawing.Point(42, 340);
+            this.tenderedLabel.Location = new System.Drawing.Point(26, 330);
             this.tenderedLabel.Name = "tenderedLabel";
             this.tenderedLabel.Size = new System.Drawing.Size(53, 13);
             this.tenderedLabel.TabIndex = 10;
@@ -140,7 +139,7 @@
             // changeLabel
             // 
             this.changeLabel.AutoSize = true;
-            this.changeLabel.Location = new System.Drawing.Point(45, 377);
+            this.changeLabel.Location = new System.Drawing.Point(26, 390);
             this.changeLabel.Name = "changeLabel";
             this.changeLabel.Size = new System.Drawing.Size(44, 13);
             this.changeLabel.TabIndex = 11;
@@ -148,7 +147,7 @@
             // 
             // calculatechangeButton
             // 
-            this.calculatechangeButton.Location = new System.Drawing.Point(42, 356);
+            this.calculatechangeButton.Location = new System.Drawing.Point(29, 355);
             this.calculatechangeButton.Name = "calculatechangeButton";
             this.calculatechangeButton.Size = new System.Drawing.Size(124, 23);
             this.calculatechangeButton.TabIndex = 12;
@@ -157,18 +156,90 @@
             // 
             // printreceiptButton
             // 
-            this.printreceiptButton.Location = new System.Drawing.Point(42, 415);
+            this.printreceiptButton.Location = new System.Drawing.Point(29, 415);
             this.printreceiptButton.Name = "printreceiptButton";
             this.printreceiptButton.Size = new System.Drawing.Size(75, 23);
             this.printreceiptButton.TabIndex = 13;
             this.printreceiptButton.Text = "Print Receipt";
             this.printreceiptButton.UseVisualStyleBackColor = true;
             // 
+            // subsInput
+            // 
+            this.subsInput.Location = new System.Drawing.Point(180, 54);
+            this.subsInput.Name = "subsInput";
+            this.subsInput.Size = new System.Drawing.Size(100, 20);
+            this.subsInput.TabIndex = 14;
+            // 
+            // chipsInput
+            // 
+            this.chipsInput.Location = new System.Drawing.Point(180, 84);
+            this.chipsInput.Name = "chipsInput";
+            this.chipsInput.Size = new System.Drawing.Size(100, 20);
+            this.chipsInput.TabIndex = 15;
+            // 
+            // cookiesInput
+            // 
+            this.cookiesInput.Location = new System.Drawing.Point(180, 115);
+            this.cookiesInput.Name = "cookiesInput";
+            this.cookiesInput.Size = new System.Drawing.Size(100, 20);
+            this.cookiesInput.TabIndex = 16;
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.AutoSize = true;
+            this.subtotalOutput.Location = new System.Drawing.Point(180, 223);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(35, 13);
+            this.subtotalOutput.TabIndex = 17;
+            this.subtotalOutput.Text = "label1";
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.AutoSize = true;
+            this.taxOutput.Location = new System.Drawing.Point(180, 253);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(35, 13);
+            this.taxOutput.TabIndex = 18;
+            this.taxOutput.Text = "label2";
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.AutoSize = true;
+            this.totalOutput.Location = new System.Drawing.Point(180, 284);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(35, 13);
+            this.totalOutput.TabIndex = 19;
+            this.totalOutput.Text = "label3";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(180, 330);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(100, 20);
+            this.tenderedInput.TabIndex = 20;
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.AutoSize = true;
+            this.changeOutput.Location = new System.Drawing.Point(180, 390);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(35, 13);
+            this.changeOutput.TabIndex = 21;
+            this.changeOutput.Text = "label4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.cookiesInput);
+            this.Controls.Add(this.chipsInput);
+            this.Controls.Add(this.subsInput);
             this.Controls.Add(this.printreceiptButton);
             this.Controls.Add(this.calculatechangeButton);
             this.Controls.Add(this.changeLabel);
@@ -177,7 +248,6 @@
             this.Controls.Add(this.taxLabel);
             this.Controls.Add(this.subtotalLabel);
             this.Controls.Add(this.calculatetotalButton);
-            this.Controls.Add(this.drinkLabel);
             this.Controls.Add(this.cookieLabel);
             this.Controls.Add(this.chipsLabel);
             this.Controls.Add(this.subLabel);
@@ -196,7 +266,6 @@
         private System.Windows.Forms.Label subLabel;
         private System.Windows.Forms.Label chipsLabel;
         private System.Windows.Forms.Label cookieLabel;
-        private System.Windows.Forms.Label drinkLabel;
         private System.Windows.Forms.Button calculatetotalButton;
         private System.Windows.Forms.Label subtotalLabel;
         private System.Windows.Forms.Label taxLabel;
@@ -205,6 +274,14 @@
         private System.Windows.Forms.Label changeLabel;
         private System.Windows.Forms.Button calculatechangeButton;
         private System.Windows.Forms.Button printreceiptButton;
+        private System.Windows.Forms.TextBox subsInput;
+        private System.Windows.Forms.TextBox chipsInput;
+        private System.Windows.Forms.TextBox cookiesInput;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Label changeOutput;
     }
 }
 
--- CashRegister/Form1.cs
+++ CashRegister/Form1.cs
@@ -10,13 +10,36 @@ using System.Windows.Forms;
 
 namespace CashRegister
 {
+   
     public partial class Form1 : Form
     {
+        //variables
+        int chips;
+        int footlong;
+        int cookies;
+        double totalcost;
+        double taxamount;
+        double totalwithtax;
+        double tenderedamount;
+        double changeamount;
+
+
+
+        //prices
+        const double FOOTLONG = 5.00;
+        const double CHIPS = 1.10;
+        const double COOKIES = 0.65;
+        const double TAX = 0.13;
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-
+        private void calculatetotalButton_Click(object sender, EventArgs e)
+        {
+            chips = Convert.ToInt32
+        }
     }
 }
